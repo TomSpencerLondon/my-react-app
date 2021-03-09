011ad2857a4f32f2fe06384d7b47c5f22e52dddc
Tom <thomas.spencer@codurance.com>
2021-03-09 10:02:27 +0000

    Add functional translation for tic tac toe

--- src/index.js
+++ src/index.js
@@ -1,75 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {useState, useEffect } from "react";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => { this.props.onClick()}}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  useEffect(() => {
+    console.log("Hello, I have changed");
+  }, [props.value])
+
+  return (
+    <button className="square" onClick={ props.onClick } >
+      { props.value }
+    </button>
+  )
 }
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const squaresClone = squares.slice();
+    squaresClone[i] = 'X';
+    setSquares(squaresClone);
   }
 
-  renderSquare(i) {
+  const renderSquare = (i) => {
     return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i) }
-    />;
+      value={squares[i]}
+      onClick={() => {
+        handleClick(i) } }
+      />;
   }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  const status = 'Next player: X';
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
       </div>
-    );
-  }
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  )
 }
-
-// ========================================
 
 ReactDOM.render(
   <Game />,
